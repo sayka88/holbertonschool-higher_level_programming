0b7306076857e39142395f34db215fac12ec99dc
sayka88 <sayka.alekperova@gmail.com>
2024-05-17 09:35:23 +0000

    6-print_comb3.py modif

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(9):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{:02d}".format(i * 10 +j))
+            print("{:02d}".format(i * 10 + j))
         else:
-            print("{:02d}".format(i * 10 +j), end=", ")
+            print("{:02d}".format(i * 10 + j), end=", ")
