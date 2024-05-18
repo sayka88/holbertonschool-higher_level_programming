257b6412b4d4a3f2acc22443e7c00e9076f5c80a
sayka88 <sayka.alekperova@gmail.com>
2024-05-18 11:20:11 +0000

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     
-    print(f"{a} {operator} {b} = {result}")
+    print("{} {} {} = {}".format(a, operator, b, result))
