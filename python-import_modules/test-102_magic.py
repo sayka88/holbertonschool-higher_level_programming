2d880148ad83ee7b3523eb867ef23beabb89e2d2
sayka88 <sayka.alekperova@gmail.com>
2024-05-18 12:46:20 +0000

    test-102_magic.py

---
+++ python-import_modules/test-102_magic.py
@@ -0,0 +1,12 @@
+import dis
+
+def magic_calculation(a, b):
+    if a < b:
+        c = a + b
+        for i in range(4, 6):
+            c += i
+        return c
+    else:
+        return a - b
+
+dis.dis(magic_calculation)
