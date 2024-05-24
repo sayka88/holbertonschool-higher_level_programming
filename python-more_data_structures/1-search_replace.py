1a711f78457890e6b5f3cc650e2ca2b39577a11a
sayka88 <sayka.alekperova@gmail.com>
2024-05-24 14:18:29 +0000

    1-search_replace.py modif

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # use list inclusion to create a new list
-    new_list = [replace if element == search else element for element in my_list]
-    return new_list
+    return [replace if num == search else num for num in my_list]
