7583470545f11a07f0fbc6686c138db572803ea8
sayka88 <sayka.alekperova@gmail.com>
2024-05-24 13:53:14 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-# Create a new matrix using nested lists
+    # Create a new matrix using nested lists
     new_matrix = [[element ** 2 for element in row] for row in matrix]
     return new_matrix
