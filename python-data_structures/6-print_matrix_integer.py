c1babcba72c030545f3d2b78d2f58acf554c4803
sayka88 <sayka.alekperova@gmail.com>
2024-05-24 10:20:37 +0000

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, element in enumerate(row):
-            if i !=0:
+            if i != 0:
                 print(" ", end="")
             print("{:d}".format(element), end="")
         print()
