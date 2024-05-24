7c20b80ac1421213dee70ae6623480f5342bb6fa
sayka88 <sayka.alekperova@gmail.com>
2024-05-24 19:24:41 +0000

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """
-    Converts a Roman numeral to an integer.
-
-    Args:
-        roman_string (str): The Roman numeral string to convert.
-
-    Returns:
-        int: The integer representation of the Roman numeral, or 0 if input is invalid.
-    """
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or type(roman_string) != str:
         return 0
-
-    roman_numerals = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    integer_value = 0
-    length = len(roman_string)
-
-    for i in range(length):
-        if i > 0 and roman_numerals[roman_string[i]] > roman_numerals[roman_string[i - 1]]:
-            integer_value += roman_numerals[roman_string[i]] - 2 * roman_numerals[roman_string[i - 1]]
+    dic_x = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    total = 0
+    for i in range(len(roman_string)):
+        if i > 0 and dic_x[roman_string[i]] > dic_x[roman_string[i - 1]]:
+            total += dic_x[roman_string[i]] - 2 * dic_x[roman_string[i - 1]]
         else:
-            integer_value += roman_numerals[roman_string[i]]
-
-    return integer_value
+            total += dic_x[roman_string[i]]
+    return total
