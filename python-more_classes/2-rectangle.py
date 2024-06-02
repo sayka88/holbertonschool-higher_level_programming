d881952f8e73443400d7f356523a76a6c8d7b820
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-02 09:09:12 +0400

    Update 2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,44 +1,60 @@
-2. Area and Perimeter
-mandatory
-Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)
-
-Private instance attribute: width:
-property def width(self): to retrieve it
-property setter def width(self, value): to set it:
-width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-if width is less than 0, raise a ValueError exception with the message width must be >= 0
-Private instance attribute: height:
-property def height(self): to retrieve it
-property setter def height(self, value): to set it:
-height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-if height is less than 0, raise a ValueError exception with the message height must be >= 0
-Instantiation with optional width and height: def __init__(self, width=0, height=0):
-Public instance method: def area(self): that returns the rectangle area
-Public instance method: def perimeter(self): that returns the rectangle perimeter:
-if width or height is equal to 0, perimeter is equal to 0
-You are not allowed to import any module
-guillaume@ubuntu:~/$ cat 2-main.py
 #!/usr/bin/python3
-Rectangle = __import__('2-rectangle').Rectangle
+"""Rectangle module.
 
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+This module contains a class that defines a rectangle.
 
-print("--")
+"""
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-guillaume@ubuntu:~/$ ./2-main.py
-Area: 8 - Perimeter: 12
---
-Area: 30 - Perimeter: 26
-guillaume@ubuntu:~/$ 
-No test cases needed
+class Rectangle():
+    """Defines a rectangle."""
 
-Repo:
+    def __init__(self, width=0, height=0):
+        """Sets the necessary attributes for the Rectangle object.
 
-GitHub repository: holbertonschool-higher_level_programming
-Directory: python-more_classes
-File: 2-rectangle.py
+        Args:
+            width (int): the width of the rectangle.
+            height (int): the height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get or set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """Get or set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """Returns the current rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the current rectangle perimeter."""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
