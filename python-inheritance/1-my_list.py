25be23655a1bafb16d8e1454e9ccce301edc4c84
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-02 11:36:09 +0400

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+"""MyList module.
+Contains a class MyList that inherits from list
+and a method that prints the sorted list.
+"""
 
 
 class MyList(list):
-    """extended version of list
-    """
+    """Defines the MyList class."""
+
     def print_sorted(self):
-        """prints the list in ascending order
-        """
-        copy = self[:]
-        copy.sort()
-        print(copy)
+        """Prints the sorted list."""
+        print(sorted(self))
