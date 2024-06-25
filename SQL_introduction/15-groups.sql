21bec3eb34c2117975d7924a75bf08e2cf77d207
Sayyara <144121567+sayka88@users.noreply.github.com>
2024-06-25 15:38:26 +0400

    Update 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- List the number of times each score appears in the second_table
-SELECT `score`, COUNT(*) AS `number` From `second_table` GROUP BY `score` ORDER BY `number` DESC;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
